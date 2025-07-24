40c0c860f2f4585f7e4e4359badf0304d069acba
Siranjeevan <smartjeevith2k@gmail.com>
2025-07-24 15:46:18 +0530

    SiteName change

--- src/Components/Header/SiteName.jsx
+++ src/Components/Header/SiteName.jsx
@@ -1,7 +1,7 @@
 function SiteName() {
   return (
     <div className="text-[1.5vw] font-bold text-[#8537ef] ml-3">
-      Hope <span className="text-black"> 3 </span>
+      Arjava <span className="text-black"> Tech </span>
     </div>
   );
 }
